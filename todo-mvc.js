0a1ffe25d3a9edc689f041197e34863d8aab2a0b
Joe Fenton <joe.e.fenton@gmail.com>
2017-07-06 14:53:31 -0400

    not sure why this was blank

---
+++ todo-mvc.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express;
+
+
+
+app.get('/api/todos', function(req, res){
+  res.json([]);
+});
+
+app.post('/api/todos', function(req, res){
+  res.json([]);
+});
+
+app.get('/api/todos', function(req, res){
+  res.json([]);
+});
+
+app.put('/api/todos', function(req, res){
+  res.json([]);
+});
+
+app.patch('/api/todos', function(req, res){
+  res.json([]);
+});
+
+app.delete('/api/todos', function(req, res){
+  res.json([]);
+});
